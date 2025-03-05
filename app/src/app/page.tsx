16aa05b372642d34ac8b9ede71065b317ab96485
sebastianlujan <1333184+sebastianlujan@users.noreply.github.com>
2025-03-04 22:14:41 -0300

    feat: add ImageUploader component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,17 @@
+import ImageUploader from "@/components/ImageUploader";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-center">
-      <header className="mb-12">
-        <h1 className="text-4xl font-bold tracking-tighter">Proof of Sauron</h1>
-        <p className="text-lg text-gray-500">"Don't trust—verify. Prove image authenticity with zero-knowledge cryptography."</p>
-      </header>
+      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start text-center">
+        <header className="mb-8">
+          <h1 className="p-4 text-5xl font-bold tracking-tighter">Proof of Sauron</h1>
+          <p className="text-lg text-gray-500">"Don't trust—verify. Prove image authenticity with zero-knowledge cryptography."</p>
+        </header>
+
+        <ImageUploader />
+        
       </main>
     </div>
   );
---
+++ app/src/components/ImageUploader.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
+export default function ImageUploader() {
+  return (
+    <main className="w-full h-full flex justify-center items-center">
+        <div className="w-full max-w-4xl">
+            <div className="border-2 border-dashed border-gray-300 rounded-xl max-xl text-center cursor-pointer transition-all duration-300 hover:border-orange-400">
+            <div className="p-12"></div>
+
+            </div>
+        </div>
+    </main>
+  );
+}
