dd23871719094b5f140d186c6c15ad08aba427a3
sebastianlujan <1333184+sebastianlujan@users.noreply.github.com>
2025-03-04 23:09:29 -0300

    feat: add framer motion effect

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,10 +4,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start text-center">
+      <main className="gap-4 row-start-2 items-center sm:items-start text-center">
         <header className="mb-8">
           <h1 className="p-4 text-5xl font-bold tracking-tighter">Proof of Sauron</h1>
-          <p className="text-lg text-gray-500">"Don't trust—verify. Prove image authenticity with zero-knowledge cryptography."</p>
+          <p className="text-md text-gray-400">Prove image authenticity with zero-knowledge cryptography"</p>
+          <p className="text-md text-gray-400"> Don't trust—verify</p> 
         </header>
 
         <ImageUploader />
--- app/src/components/ImageUploader.tsx
+++ app/src/components/ImageUploader.tsx
@@ -2,16 +2,19 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function ImageUploader() {
   return (
-    <main className="w-full h-full flex justify-center items-center">
-        <div className="w-full max-w-4xl">
-            <div className="border-2 border-dashed border-gray-300 rounded-xl max-xl text-center cursor-pointer transition-all duration-300 hover:border-orange-400">
-            <div className="p-12"></div>
-
-            </div>
+    <motion.div
+        className="flex flex-col justify-center items-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3 }}   
+    >
+        <div className="border-2 border-dashed border-gray-300 rounded-xl max-w-xl text-center cursor-pointer transition-all duration-300 hover:border-orange-400 break-all p-12 ">
         </div>
-    </main>
+    </motion.div>
+       
   );
 }
