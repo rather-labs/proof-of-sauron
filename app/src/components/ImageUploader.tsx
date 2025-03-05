2096986c8e601c88c154c05688997aee9b99abed
sebastianlujan <1333184+sebastianlujan@users.noreply.github.com>
2025-03-05 03:53:43 -0300

    feat: update ImageUploader implemented useDragzone hook

--- app/src/components/ImageUploader.tsx
+++ app/src/components/ImageUploader.tsx
@@ -1,45 +1,33 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
-import { useDropzone } from 'react-dropzone';
+import { useState, useCallback } from "react";
 import { Upload, Image as ImageIcon } from "lucide-react";
+import { useDropzone } from "react-dropzone";
 import { motion } from "framer-motion";
 
 
 
 interface ImageUploaderProps {
-    onFileUpload: (file: File) => void;
+    onImageUpload: (file: File) => void;
 }
-export default function ImageUploader({ onFileUpload }: ImageUploaderProps) {
-    const [isDragging, setIsDragging] = useState(false);
-
-    const handleDragOver = (e: React.DragEvent) => {
-        setIsDragging(true);
-    };
-
-    const handleDragLeave = (e: React.DragEvent) => {
-        setIsDragging(false)
-    };
-
-
-    const handleDrop = (e: React.DragEvent) => {
-        e.preventDefault();
-        setIsDragging(false);
-
-        const file = e.dataTransfer.files[0];
-        let isValidFile = e.dataTransfer.files.length != 0;
-        isValidFile &&= file && file.type.startsWith("image/");
-
-        if(isValidFile) {   
-            const fileReader = new FileReader();
-            // Handle file reading here if needed
-            onFileUpload(file);
-            console.log(file);
-
+export default function ImageUploader({ onImageUpload }: ImageUploaderProps) {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const file = acceptedFiles[0];
+        if (file) {
+            const url = URL.createObjectURL(file);
+            onImageUpload(file);
         }
-        console.log(file );
-    };
+      }, [onImageUpload]);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: { 
+            "image/*": [".png", ".jpg", ".jpeg", ".gif", ".webp"] 
+        },
+        maxFiles: 1,
+        multiple: false,
+    });
 
     return (
         <motion.div
@@ -47,17 +35,13 @@ export default function ImageUploader({ onFileUpload }: ImageUploaderProps) {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3 }}
-            whileHover={{ scale: 1.05 }} 
-            onDragOver={handleDragOver}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-            
-            >
+            whileHover={{ scale: 1.05 }}>
 
+            <input {...getInputProps()} />
             <div className={`
                 border-2 border-dashed border-gray-300 rounded-xl max-w-xl text-center 
                 cursor-pointers transition-all duration-300 hover:border--400 break-all p-12 
-                ${isDragging
+                ${isDragActive
                     ? "border-white bg-white/10" 
                     : "border-gray-600 hover:border-white/5"}`}>
                 <motion.div whileHover={{ scale: 1.1 }}>
