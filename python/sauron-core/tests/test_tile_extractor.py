67cc1562e0926fc67c9dcf19d409ef2d6828acec
sebastianlujan <1333184+sebastianlujan@users.noreply.github.com>
2025-03-18 02:16:38 -0300

    test: add extract tile for middle and bottom right

--- python/sauron-core/tests/test_tile_extractor.py
+++ python/sauron-core/tests/test_tile_extractor.py
@@ -61,3 +61,19 @@ def test_rectangle_top_letf_tile(extractor, rectangle_image):
     
     top_left = extractor.extract_tile(img_array, 0, 0, tile_width, tile_height)
     assert top_left['position'] == (0, 0, 64, 64)
+
+def test_rectangle_middle_tile(extractor, rectangle_image):
+    """Test extraction of middle tile"""
+    img_array = np.array(rectangle_image)
+    n_cols, n_rows, tile_width, tile_height = extractor.compute_tile_dimensions(rectangle_image)
+    
+    middle = extractor.extract_tile(img_array, 4, 2, tile_width, tile_height)
+    assert middle['position'] == (128, 256, 192, 320)
+
+def test_rectangle_bottom_right_tile(extractor, rectangle_image):
+    """Test extraction of bottom-right tile"""
+    img_array = np.array(rectangle_image)
+    n_cols, n_rows, tile_width, tile_height = extractor.compute_tile_dimensions(rectangle_image)
+    
+    bottom_right = extractor.extract_tile(img_array, 7, 3, tile_width, tile_height)
+    assert bottom_right['position'] == (192, 448, 256, 512)
