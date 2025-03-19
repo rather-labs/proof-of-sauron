fe820de6678ffe7af3da42587a6c864fbebbc561
sebastianlujan <1333184+sebastianlujan@users.noreply.github.com>
2025-03-19 15:45:00 -0300

    test: improve compute image helper

--- python/sauron-core/tests/test_entropy.py
+++ python/sauron-core/tests/test_entropy.py
@@ -31,6 +31,11 @@ def uniform_image():
 def analizer():
     return EntropyAnalyzer()
 
+def _compute_image_entropy(image, analyzer):
+    image = image.convert('L')
+    image_array = np.array(image)
+    return analyzer.compute_entropy(image_array)
+
 @pytest.mark.parametrize("x, center, steepness, expected", [
     (0, 0.5, 10, 1 / (1 + np.exp(5))),
     (0.5, 0.5, 10, 0.5),
@@ -39,28 +44,19 @@ def analizer():
 def test_normalized_sigmoid(x, center, steepness, expected, analizer):
     assert np.isclose(analizer.normalized_sigmoid(x, center, steepness), expected, atol=1e-5)
 
+
 def test_compute__entropy_uniform_image(analizer, uniform_image):
     """ Test that the entropy is computed correctly in a uniform image"""
-
-    image_array = np.array(uniform_image)
-    entropy = analizer.compute_entropy(image_array)
+    entropy = _compute_image_entropy(uniform_image, analizer)
     assert entropy == 0
+
 
 def test_compute_entropy_real_image(analizer, real_image):
     """ Test that the entropy is computed correctly in a real image"""
-
-    real_image = real_image.convert('L')
-    image_array = np.array(real_image)
-
-    entropy = analizer.compute_entropy(image_array)
+    entropy = _compute_image_entropy(real_image, analizer)
     assert 0.5 <= entropy <= 1
 
 def test_compute_entropy_fake_image(analizer, fake_image):
     """ Test that the entropy is computed correctly in a real image"""
-
-    fake_image = fake_image.convert('L')
-    image_array = np.array(fake_image)
-
-    entropy = analizer.compute_entropy(image_array)
-    print(f"entropy: ", entropy)
+    entropy = _compute_image_entropy(fake_image, analizer)
     assert 0.5 <= entropy <= 1
